1c72aa3f16353905d6532a9008e1b27409c0e852
Maciej Bak <wsciekly.maciek@gmail.com>
2021-03-06 23:44:50 +0100

    print

--- scripts/calculate-tin.py
+++ scripts/calculate-tin.py
@@ -542,7 +542,7 @@ def main():
 
         pool.map(gf, conditions)
 
-    print(sample_TINS_per_transcript[sample_TINS_per_transcript.keys()[0]])
+    print(sample_TINS_per_transcript[sorted(sample_TINS_per_transcript.keys()[2]]))
     for ex in sorted(sample_TINS_per_transcript.keys()):
         val = map(str, sample_TINS_per_transcript[ex])
         print(
