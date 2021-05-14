0fa20e956b979ac2a2e5218d581fc60603bc4119
Foivos Gypas <fgypas@gmail.com>
2021-05-14 16:40:34 +0200

    Read in requirements.txt in setup.py. Be more lenient in the versions.

--- setup.py
+++ setup.py
@@ -4,15 +4,8 @@ from setuptools import setup
 if sys.version_info < (3, 6):
     sys.exit('Sorry, tin-score-calculation requires Python >= 3.6')
 
-requirements = [
-    "bx-python>=0.8.10",
-    "guppy3>=3.1.0",
-    "matplotlib>=3.3.4",
-    "pandas>=1.2.3",
-    "psutil>=5.8.0",
-    "pysam>=0.16.0.1",
-    "RSeQC>=3.0.1",
-]
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='tin-score-calculation',
@@ -21,7 +14,7 @@ setup(
     url='',
     include_package_data=True,
     scripts=['scripts/calculate-tin.py', 'scripts/merge-tin.py', 'scripts/plot-tin.py'],
-    install_requires=requirements,
+    install_requires=required,
     keywords='tin-score-calculation',
     classifiers=[
         'Programming Language :: Python :: 3.6',
